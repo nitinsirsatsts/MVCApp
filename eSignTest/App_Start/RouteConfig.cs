0ae53457e91aadc9d876f00ba17f0ec5328e9df9
nitinsirsatsts <nitin.sirsat@sts.in>
2021-11-09 17:32:03 +0530

    Test

--- eSignTest/App_Start/RouteConfig.cs
+++ eSignTest/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace eSignTest
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            //Test
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
